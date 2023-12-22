f85573bf1a196be989ddea342681cc787d66267e
Bobbie Goede <bobbiegoede@gmail.com>
2023-12-22 10:55:38 +0100

    chore: add component workaround

---
+++ components/TranslationComponent.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const text = computed(() => {
+	const i18n = useNuxtApp().$i18n;
+	return i18n.t("seventeen");
+});
+</script>
+
+<template>
+	<div>
+		{{ JSON.parse(JSON.stringify(text)) }}
+	</div>
+</template>
---
+++ pages/no-leak-component.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import useFooStore from "~/store/foo";
+import TranslationComponent from "~/components/TranslationComponent.vue";
+
+const fooStore = useFooStore();
+await fooStore.loadData();
+</script>
+
+<template>
+	<div>
+		<TranslationComponent />
+	</div>
+</template>
